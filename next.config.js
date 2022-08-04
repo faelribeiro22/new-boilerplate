74a1d8b8365341136531db9c64abeb16ebf569fb
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2022-08-04 03:57:01 -0300

    feat: change babel to SWC

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = withPWA({
+  swcMinify: true,
+  compiler: {
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
