bde32a9d9d8f46f6bd9a059a6587e229d2895068
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2023-01-17 12:09:04 -0300

    fix: change next.config to pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
 
 const nextConfig = withPWA({
   swcMinify: true,
   compiler: {
     styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
   },
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   reactStrictMode: true
